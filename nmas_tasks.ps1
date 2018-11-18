15a73c9a75682c6b25f6a07006fdd1cd30aee4a6
Nate Ravid <nravid@gothamtg.net>
2018-11-18 09:51:28 -0500

    Create nmas_tasks.ps1

---
+++ nmas_tasks.ps1
@@ -0,0 +1,4 @@
+﻿$credential = Get-Credential
+
+$uri = "http://192.168.137.25/nitro/v2/config/managed_device"
+$nmasdevices = Invoke-RestMethod -uri $uri -Credential $credential | select managed_device -ExpandProperty managed_device | select type, ip_address, instance_state, ha_master_state | Where-Object {($_.type -eq "nsvpx") -and ($_.instance_state -eq "Up") -and ($_.ha_master_state -eq "Primary")}
