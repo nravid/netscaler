3de1ee7b59e80ca3f1e3e45f0a0195990e52611b
Nate Ravid <nravid@gothamtg.com>
2024-03-06 08:21:49 -0500

    Create bot-hits.sh

---
+++ awk_scripts/bot-hits.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh
+
+#awk '/default BOT Message/ {print$18} END {print q}' /var/log/ns.log | sort | uniq > /var/tmp/bothitsuniq.txt
+
+#awk '/bind policy patset pat/ {print $5} END {print q}' /nsconfig/ns.conf | sort > /var/tmp/bindpat.txt
+
+#diff -y /var/tmp/bothitsuniq.txt /var/tmp/bindpat.txt | grep '<'
+
+
+diff -y <(awk '/default BOT Message/ {print$18} END {print q}' /var/log/ns.log | sort | uniq) <(awk '/bind policy patset pat/ {print $5} END {print q}' /nsconfig/ns.conf | sort) | grep '<'
